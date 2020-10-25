1142fec74f33da44e4ebd07fa5966c77ab379225
kga <kga@live.dk>
2020-10-25 21:28:28 +0100

    started

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import  {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {Home} from './pages/Home'
+import {Layout} from './layout/Layout'
+import {NavigationBar} from './layout/Navigationbar'
+import {NotFound} from './pages/NotFound'
+import {ConfigurationList} from './configuration/ConfigurationList'
+import {ConfigurationForm} from './configuration/ConfigurationForm'
 
-function App() {
-  return (
-    <div className="App">
-    <Router>
+class App extends Component {
+
+  render(){
+    return (
+      <div className="App">
+        <Router>
+          <NavigationBar />
+          <Layout>
             <Switch>
               <Route exact path="/" component={Home} />¨
               <Route path="/configuration" component={ConfigurationList}/>
+              <Route path="/configuration/create" component= {ConfigurationForm} />
               <Route component= {NotFound} />
             </Switch>
-    </Router>
-    </div>
-  );
+          </Layout>
+        </Router>
+      </div>
+    );  
+  }
 }
 
 export default App;
